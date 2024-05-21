299f233387b072dbef42c2f7e464c0b223438876
hrishd <hrishd@abv.bg>
2024-05-21 15:04:37 +0300

    logo at the middle

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,18 @@
 <x-layout>
     <main>
+        <div>
+            <div class="container">
+                <div class="row mb-3">
+                    <div class="col d-flex justify-content-center">
+                        <img src="{{ asset('img/Logo_Transparent.png') }}" alt=""  class="body-logo">
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col d-flex justify-content-center">
+                        zdr
+                    </div>
+                </div>
+            </div>
+        </div>
     </main>
 </x-layout>
