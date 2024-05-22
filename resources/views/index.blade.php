df9b16eccabed0eae97ec9704f15d21fd1c1a618
hrishd <hrishd@abv.bg>
2024-05-22 11:03:25 +0300

    removed not needed div

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,16 +1,14 @@
 <x-layout>
     <main>
-        <div>
-            <div class="container">
-                <div class="row mb-3">
-                    <div class="col d-flex justify-content-center">
-                        <img src="{{ asset('img/Logo_Transparent.png') }}" alt=""  class="body-logo">
-                    </div>
+        <div class="container">
+            <div class="row mb-3">
+                <div class="col d-flex justify-content-center ">
+                    <img src="{{ asset('img/Logo_Transparent.png') }}" alt="" class="body-logo">
                 </div>
-                <div class="row mb-3">
-                    <div class="col d-flex justify-content-center">
-                        zdr
-                    </div>
+            </div>
+            <div class="row mb-3">
+                <div class="col d-flex justify-content-center">
+                    sliders and img box here
                 </div>
             </div>
         </div>
