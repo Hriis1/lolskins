3c2989cf422437edea3242739a49ccabe855eff4
hrishd <hrishd@abv.bg>
2024-05-26 23:06:27 +0300

    function to get skins of a champ

--- public/js/getLoLChampsData.js
+++ public/js/getLoLChampsData.js
@@ -20,6 +20,32 @@ async function getLoLChampsData() {
     }
 }
 
+async function getLoLChampSkins(champName) {
+    var settings = {
+        "url": "https://ddragon.leagueoflegends.com/cdn/14.10.1/data/en_US/champion/" + champName + ".json",
+        "method": "GET",
+        "timeout": 0,
+    };
+
+    try {
+        const response = await new Promise((resolve, reject) => {
+            $.ajax(settings).done(function (response) {
+                resolve(response['data'][champName]['skins']);
+            }).fail(function (jqXHR, textStatus, errorThrown) {
+                reject(errorThrown);
+            });
+        });
+
+        //Remove the 1st element of the response since it is the default skin
+        response.shift();
+
+        return response;
+    } catch (error) {
+        console.error('Error fetching LoL Champ Skins data:', error);
+        return null;
+    }
+}
+
 $(document).ready(async function () {
     var lolChampsData = await getLoLChampsData();
     if (lolChampsData) {
@@ -28,5 +54,10 @@ $(document).ready(async function () {
             const championDiv = $('<div></div>').addClass('dropdown-choice').text(champion.name);
             container.append(championDiv);
         });
+    }
+
+    const skins = await getLoLChampSkins('Aatrox');
+    if (skins) {
+        console.log(skins);
     }
 });
