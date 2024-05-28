bb06191e77cdf4566235a68f01dc9208cfac828b
hrishd <hrishd@abv.bg>
2024-05-28 10:23:40 +0300

    set default img background to empty img

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,40 +1,40 @@
 <x-layout>
-        <div class="container">
-            <div class="row mb-3">
-                <div class="col d-flex justify-content-center ">
-                    <img src="{{ asset('img/Logo_Transparent.png') }}" alt="" class="body-logo">
-                </div>
+    <div class="container">
+        <div class="row mb-3">
+            <div class="col d-flex justify-content-center ">
+                <img src="{{ asset('img/Logo_Transparent.png') }}" alt="" class="body-logo">
             </div>
-            <div class="row mb-3">
-                <div class="col d-flex justify-content-center">
-                    <div class="main-content d-flex justify-content-center align-items-center p-5 mb-5">
-                        <div class="dropdown pe-5" id="champDropDown">
-                            <input type="text" class="myInput" id="champInput" placeholder="Select champ..">
-                            <div class="dropdown-content dropdown-content-champions">
-                            </div>
+        </div>
+        <div class="row mb-3">
+            <div class="col d-flex justify-content-center">
+                <div class="main-content d-flex justify-content-center align-items-center p-5 mb-5">
+                    <div class="dropdown pe-5" id="champDropDown">
+                        <input type="text" class="myInput" id="champInput" placeholder="Select champ..">
+                        <div class="dropdown-content dropdown-content-champions">
                         </div>
+                    </div>
 
-                        <div class="dropdown uninteractable pe-5" id="skinDropDown">
-                            <input type="text" class="myInput" id="skinInput" placeholder="Select skin.." skin-url="">
-                            <div class="dropdown-content dropdown-content-skins">
-                            </div>
+                    <div class="dropdown uninteractable pe-5" id="skinDropDown">
+                        <input type="text" class="myInput" id="skinInput" placeholder="Select skin.." skin-url="">
+                        <div class="dropdown-content dropdown-content-skins">
                         </div>
+                    </div>
 
-                        <div class="skin-container">
-                            <div class="skin-image-container">
-                                <img src="path/to/your/image.png" alt="">
-                            </div>
-                            <div class="skin-text-container">
-                                <h2 class="skin-title">Battle Queen Katarina</h2>
-                                <p class="description">Busted skin! Lorem ipsum dolor sit amet consectetur adipisicing
-                                    elit.
-                                    Fuga quod debitis consectetur corrupti nemo. Veritatis, amet?.</p>
-                            </div>
+                    <div class="skin-container">
+                        <div class="skin-image-container">
+                            <img src="{{ asset('img/empty.png') }}" alt="">
+                        </div>
+                        <div class="skin-text-container">
+                            <h2 class="skin-title">Battle Queen Katarina</h2>
+                            <p class="description">Busted skin! Lorem ipsum dolor sit amet consectetur adipisicing
+                                elit.
+                                Fuga quod debitis consectetur corrupti nemo. Veritatis, amet?.</p>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
 </x-layout>
 <script src="{{asset('js/searchableDropdown.js')}}"></script>
 <script src="{{asset('js/getLoLChampsData.js')}}"></script>
