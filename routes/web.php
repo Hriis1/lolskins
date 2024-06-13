01161a332b31fbf114d5af61dd926260f663d686
hrishd <hrishd@abv.bg>
2024-06-13 23:18:24 +0300

    admin redirects based on if a user is logged

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,13 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\UserController;
 
-//---------------------------Main page---------------------------
+//---------------------------Main page----------------------------
 Route::get('/', function () {
     return view('index');
 })->name("main");
 
 
-//---------------------------Users---------------------------
+//---------------------------Users--------------------------------
 //Log in form
 Route::get('/users/loginForm', [UserController::class, 'logInForm'])->name("logInForm");
 //Log in
@@ -21,3 +21,15 @@ Route::get('/users/signupForm', [UserController::class, 'signUpForm'])->name("si
 Route::post('/users/signup', [UserController::class, 'signUp'])->name("signUp");
 //Log out
 Route::post('/users/logout', [UserController::class, 'logOut'])->name("logOut");
+
+
+//---------------------------Admin stuff---------------------------
+Route::get('/admin', function () {
+
+    if (session()->has('user_id')) {
+        //if there is a user set and he is an admin
+        return redirect('/admin/main');
+    } else {
+        return redirect('/admin/loginPage');
+    }
+})->name('admin');
