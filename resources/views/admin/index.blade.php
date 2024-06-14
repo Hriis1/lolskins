41782a79c72e61fd7d0c78e199ea53ebc38dc072
hrishd <hrishd@abv.bg>
2024-06-14 18:22:08 +0300

    main content height and margin

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,3 +1,10 @@
 <x-adminLayout>
-   <h1>zdr kopele admin</h1> 
+    <div class="container mt-rem-8 mb-5 p-5 main-content-admin">
+        <div class="row">
+            <div class="col d-flex justify-content-center">
+                zdr kopele admin <br> <br>
+                asdasd
+            </div>
+        </div>
+    </div>
 </x-adminLayout>
