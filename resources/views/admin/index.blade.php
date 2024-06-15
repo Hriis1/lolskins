0941dca295153e4ba27d2bf077f1c686b5e6a9f1
hrishd <hrishd@abv.bg>
2024-06-15 14:46:47 +0300

    add datatable (not connectiong to datatables)

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,10 +1,29 @@
 <x-adminLayout>
     <div class="container mt-rem-8 mb-5 p-5 main-content-admin">
-        <div class="row">
-            <div class="col d-flex justify-content-center">
-                zdr kopele admin <br> <br>
-                asdasd
+        <div class="row justify-content-center">
+            <div class="col-lg-10 col-sm-12">
+                <table id="dataSkins" class="table table-striped" style="width:100%">
+                    <thead style="width: 100%;">
+                        <tr>
+                            <th>Name</th>
+                            <th>Usable</th>
+                            <th>Opinion</th>
+                            <th>Rating</th>
+                            <th>Best skin</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
 </x-adminLayout>
+<script src="{{asset('js/skinsTable.js')}}"></script>
